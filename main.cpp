5032b5621001bfdb949a7f73575485c70d87165b
Tracy-115 <kmin984@insite.4cd.edu>
2025-10-26 19:42:07 -0700

    started coding on inserting value in the middle

--- main.cpp
+++ main.cpp
@@ -31,7 +31,6 @@ int main() {
     auto end = high_resolution_clock::now();
     auto durationVECTOR = duration_cast<microseconds>(end - start);
     long long vectorT = durationVECTOR.count();
-    
     //TWO
     list<string> lists;
     start = high_resolution_clock::now();
@@ -41,8 +40,6 @@ int main() {
     end = high_resolution_clock::now();
     auto durationLIST = duration_cast<microseconds>(end - start);
     long long listT = durationLIST.count();
-    
-
     //THREE
     set<string> SET;
     start = high_resolution_clock::now();
@@ -56,24 +53,48 @@ int main() {
     cout << "Read" << "      " << vectorT << "      " << listT << "      " << setT << endl;
 
     //ONE
-    auto start = high_resolution_clock::now();
+     start = high_resolution_clock::now();
     vector.sort(vector.being(), vector.end())
-    auto end = high_resolution_clock::now();
+     end = high_resolution_clock::now();
     auto durationSORT = duration_cast<microseconds>(end - start);
     long long vectorS = durationSORT.count();
-    
     //TWO
     start = high_resolution_clock::now();
     list.sort();
     end = high_resolution_clock::now();
     auto durationSORT = duration_cast<microseconds>(end - start);
     long long listS= durationSORT.count();
-    
-
     //THREE
     long long setSORT = -1;
 
     cout << "Set" << "      " << vectorS << "      " << listS << "      " << setSORT << endl;
+
+    string setValue ="TESTCODE";
+    //ONE
+    vector<string> vectorIns=vectorI;
+    start = high_resolution_clock::now();
+    vectorIns.insert(vectorIns.begin()+vectroIns.size()/2, setValue);
+    start = high_resolution_clock::now();
+    auto durationVECTOR = duration_cast<microseconds>(end - start);
+    long long vectorT = durationVECTOR.count();
+    //TWO
+    list<string> listIns = listI;
+    auto listMiddle = listIns.begin();
+    advance(listMiddle, listIns.size() / 2);
+    start = high_resolution_clock::now();
+    listIns.insert(listMiddle,setValue);
+    end = high_resolution_clock::now();
+    auto durationLIST = duration_cast<microseconds>(end - start);
+    long long listT = durationLIST.count();
+    //THREE
+    set<string> SET;
+    start = high_resolution_clock::now();
+    for (const auto &c : data){
+        SET.insert(c);
+    }
+    end = high_resolution_clock::now();
+    auto durationSET = duration_cast<microseconds>(end - start);
+    long long setT = durationSET.count();
     return 0;
 }
 
